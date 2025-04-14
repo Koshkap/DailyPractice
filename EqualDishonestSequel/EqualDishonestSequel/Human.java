aed5baf097b36a31780c7922a243323c6f5d71e5
Yunus G <nunus.trains@gmail.com>
2025-04-14 10:09:54 -0400

    Delete EqualDishonestSequel/EqualDishonestSequel directory

--- EqualDishonestSequel/EqualDishonestSequel/Human.java
+++
@@ -1,9 +0,0 @@
-
-// TODO: Define a final class Human that extends Mammal
-// - Constructor takes name and passes name, true, "omnivore", 2, "Hello!" to
-// super
-public final class Human extends Mammal {
-    public Human(String name) {
-        super(name, true, "omnivore", 2, "Hello!");
-    }
-}
